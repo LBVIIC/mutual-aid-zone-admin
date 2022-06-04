0178b6bd1f53853f52a605b89bd9e90fc2aaafee
izhichao <43055550@qq.com>
2022-06-04 10:26:38 +0800

    feat: 完成用户新增与编辑的表单验证

--- src/composables/useUser.ts
+++ src/composables/useUser.ts
@@ -1,4 +1,4 @@
-import { ElMessage, FormInstance } from 'element-plus';
+import { ElMessage, FormInstance, FormRules } from 'element-plus';
 import { reactive, ref } from 'vue';
 import { addUser, deleteUser, editUser, getUser, getUsers } from '../api/user';
 
@@ -11,22 +11,16 @@ export const useUser = () => {
     phone: string;
     address: string;
     balance: number;
-    role: string;
+    role: string | number;
   }
 
   const addUserFormVisible = ref(false);
   const editUserFormVisible = ref(false);
+  const addFormRef = ref<FormInstance>();
+  const editFormRef = ref<FormInstance>();
 
   const userList = ref<User[]>([]);
-
-  const addModel = reactive({
-    username: '',
-    password: '',
-    email: '',
-    phone: ''
-  });
-
-  const editModel = ref({
+  const userModel = ref<User>({
     _id: '',
     username: '',
     password: '',
@@ -37,13 +31,34 @@ export const useUser = () => {
     role: 0
   });
 
+  // 表单校验规则
+  const rules = reactive<FormRules>({
+    username: [
+      { required: true, message: '用户名为必填项', trigger: 'blur' },
+      { min: 2, max: 12, message: '用户名长度应为2-12个字符', trigger: 'blur' }
+    ],
+    email: [
+      { required: true, message: '邮箱为必填项', trigger: 'blur' },
+      { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+    ],
+    phone: [
+      { required: true, message: '手机号为必填项', trigger: 'blur' },
+      { pattern: /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/, message: '请输入正确的手机号', trigger: 'blur' }
+    ],
+    password: [
+      { required: true, message: '密码为必填项', trigger: 'blur' },
+      { pattern: /^[a-zA-Z0-9_!]{6,16}$/, message: '密码长度应为6-16个字符', trigger: 'change' }
+    ],
+    editPassword: [{ pattern: /^[a-zA-Z0-9_!]{6,16}$/, message: '密码长度应为6-16个字符', trigger: 'change' }]
+  });
+
   // 获取用户列表
   const handleUserList = async () => {
     const { data: res } = await getUsers();
     if (res.errno === 0) {
       userList.value = res.data;
       userList.value.forEach((item) => {
-        if (parseInt(item.role) === 0) {
+        if (item.role === 0) {
           item.role = '普通用户';
         } else {
           item.role = '管理员';
@@ -53,51 +68,56 @@ export const useUser = () => {
   };
 
   // 新增用户
-  const handleAdd = async (formEl: FormInstance | undefined) => {
+  const handleAdd = (formEl: FormInstance | undefined) => {
+    addUserFormVisible.value = true;
+    formEl.resetFields();
+  };
+
+  // 编辑用户
+  const handleEdit = async (row: any) => {
+    editUserFormVisible.value = true;
+    const { data: res } = await getUser(row._id);
+    res.data.password = '';
+    userModel.value = res.data;
+  };
+
+  // 提交表单
+  const handleSubmit = async (formEl: FormInstance | undefined, type: string) => {
     if (!formEl) return;
-    await formEl.validate((valid, fields) => {
+    await formEl.validate(async (valid, fields) => {
       if (valid) {
-        // const { username, password, email, phone } = addModel;
-        // const { data: res } = await addUser(username, password, email, phone);
-        // if (res.errno === 0) {
-        //   addUserFormVisible.value = false;
-        //   handleUserList();
-        // } else {
-        //   ElMessage.error(res.msg);
-        // }
-        console.log('ok');
-        
-      } else {
-        console.log('error submit!', fields);
+        const { username, password, email, phone } = userModel.value;
+        if (type === 'add') {
+          const { data: res } = await addUser(username, phone, email, password);
+          if (res.errno === 0) {
+            addUserFormVisible.value = false;
+            ElMessage({ message: '添加成功', type: 'success' });
+            formEl.resetFields();
+            handleUserList();
+          } else {
+            ElMessage.error(res.msg);
+          }
+        } else if (type === 'edit') {
+          const { _id, address, balance, role } = userModel.value;
+          const { data: res } = await editUser(_id, username, password, email, phone, address, balance, role as number);
+          if (res.errno === 0) {
+            editUserFormVisible.value = false;
+            ElMessage({ message: res.msg, type: 'success' });
+            handleUserList();
+          } else {
+            ElMessage.error(res.msg);
+          }
+        }
       }
     });
   };
 
-  // 编辑用户
-  const handleShowEdit = async (row: any) => {
-    editUserFormVisible.value = true;
-    const { data: res } = await getUser(row._id);
-    res.data.password = '';
-    editModel.value = res.data;
-  };
-
-  const handleEdit = async () => {
-    const { _id, username, password, email, phone, address, balance, role } = editModel.value;
-    const { data: res } = await editUser(_id, username, password, email, phone, address, balance, role);
-    if (res.errno === 0) {
-      editUserFormVisible.value = false;
-      ElMessage({
-        message: res.msg,
-        type: 'success'
-      });
-      handleUserList();
-    } else {
-      ElMessage.error(res.msg);
-    }
-  };
-
   // 删除用户
   const handleDelete = async (row: any) => {
+    if (row.username === 'admin') {
+      ElMessage.error('默认管理员不可删除');
+      return;
+    }
     const { data: res } = await deleteUser(row._id);
     if (res.errno === 0) {
       ElMessage({
@@ -109,15 +129,17 @@ export const useUser = () => {
   };
 
   return {
+    rules,
     addUserFormVisible,
     editUserFormVisible,
-    addModel,
-    editModel,
+    addFormRef,
+    editFormRef,
+    userModel,
     userList,
     handleUserList,
-    handleShowEdit,
+    handleAdd,
     handleEdit,
-    handleDelete,
-    handleAdd
+    handleSubmit,
+    handleDelete
   };
 };
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>用户管理</h2>
-  <el-button type="primary" @click="addUserFormVisible = true">新增用户</el-button>
+  <el-button type="primary" @click="handleAdd(addFormRef)">新增用户</el-button>
 
   <!-- 表格 -->
   <el-table :data="userList" stripe height="460" style="width: 100%" class="table">
@@ -12,14 +12,8 @@
     <el-table-column prop="role" label="权限" />
     <el-table-column label="操作" width="140" fixed="right">
       <template #default="scope">
-        <el-button size="small" @click="handleShowEdit(scope.row)">编辑</el-button>
-        <el-popconfirm
-          title="确认要删除吗？"
-          placement="top-end"
-          @confirm="handleDelete(scope.row)"
-          confirm-button-text="确认"
-          cancel-button-text="取消"
-        >
+        <el-button size="small" @click="handleEdit(scope.row)">编辑</el-button>
+        <el-popconfirm title="确认要删除吗？" @confirm="handleDelete(scope.row)" confirm-button-text="确认" cancel-button-text="取消">
           <template #reference>
             <el-button size="small" type="danger">删除</el-button>
           </template>
@@ -29,85 +23,80 @@
   </el-table>
 
   <el-dialog v-model="addUserFormVisible" title="新增用户">
-    <el-form ref="addFormRef" :model="addModel" label-width="80px" :rules="rules">
+    <el-form ref="addFormRef" :model="userModel" label-width="80px" :rules="rules">
       <el-form-item label="用户名" prop="username">
-        <el-input v-model="addModel.username" />
+        <el-input v-model="userModel.username" />
       </el-form-item>
-      <el-form-item label="邮箱">
-        <el-input v-model="addModel.email" />
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="userModel.email" />
       </el-form-item>
-      <el-form-item label="手机号">
-        <el-input v-model="addModel.phone" />
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="userModel.phone" />
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="addModel.password" />
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="userModel.password" show-password />
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="addUserFormVisible = false">关闭</el-button>
-        <el-button type="primary" @click="handleAdd(addFormRef)">提交</el-button>
+        <el-button type="primary" @click="handleSubmit(addFormRef, 'add')">提交</el-button>
       </span>
     </template>
   </el-dialog>
 
   <el-dialog v-model="editUserFormVisible" title="修改用户">
-    <el-form :model="editModel" label-width="80px">
-      <el-form-item label="用户名">
-        <el-input v-model="editModel.username" />
+    <el-form ref="editFormRef" :model="userModel" label-width="80px" :rules="rules">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="userModel.username" :disabled="userModel.username === 'admin'" />
       </el-form-item>
-      <el-form-item label="邮箱">
-        <el-input v-model="editModel.email" />
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="userModel.email" />
       </el-form-item>
-      <el-form-item label="手机号">
-        <el-input v-model="editModel.phone" />
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="userModel.phone" />
       </el-form-item>
       <el-form-item label="地址">
-        <el-input v-model="editModel.address" />
+        <el-input v-model="userModel.address" />
       </el-form-item>
       <el-form-item label="余额">
-        <el-input-number v-model="editModel.balance" :step="10" :min="0" :max="9999" />
+        <el-input-number v-model="userModel.balance" :step="10" :min="0" :max="9999" />
       </el-form-item>
       <el-form-item label="权限">
-        <el-radio-group v-model="editModel.role">
-          <el-radio :label="0" size="large">普通用户</el-radio>
-          <el-radio :label="1" size="large">管理员</el-radio>
+        <el-radio-group v-model="userModel.role">
+          <el-radio :label="0">普通用户</el-radio>
+          <el-radio :label="1">管理员</el-radio>
         </el-radio-group>
       </el-form-item>
-
-      <el-form-item label="密码">
-        <el-input v-model="editModel.password" />
+      <el-form-item label="密码" prop="editPassword">
+        <el-input v-model="userModel.password" type="password" show-password />
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="editUserFormVisible = false">取消</el-button>
-        <el-button type="primary" @click="handleEdit">提交</el-button>
+        <el-button type="primary" @click="handleSubmit(editFormRef, 'edit')">提交</el-button>
       </span>
     </template>
   </el-dialog>
 </template>
 
 <script lang="ts" setup>
-import { FormInstance } from 'element-plus';
-import { reactive, ref } from 'vue';
 import { useUser } from '../composables/useUser';
 
-const addFormRef = ref<FormInstance>();
-const rules = reactive({
-  username: { required: true, message: 'Please input Activity name' }
-});
 const {
-  userList,
-  editModel,
-  addModel,
+  rules,
   addUserFormVisible,
   editUserFormVisible,
+  addFormRef,
+  editFormRef,
+  userModel,
+  userList,
   handleUserList,
-  handleShowEdit,
+  handleAdd,
   handleEdit,
-  handleDelete,
-  handleAdd
+  handleSubmit,
+  handleDelete
 } = useUser();
 
 handleUserList();
